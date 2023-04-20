2f273a1bcd4e2976553150367a912f5cb2b158c5
hugojohansson <carlhugojohansson@outlook.com>
2023-04-21 00:09:53 +0200

    Created Events and result files, Event class is for only executing the data once, Result class is to manage the state of loading moviedetail fragment

--- app/src/main/java/com/example/projektarb/ui/details/MovieDetailsRepository.kt
+++ app/src/main/java/com/example/projektarb/ui/details/MovieDetailsRepository.kt
@@ -3,11 +3,32 @@ package com.example.projektarb.ui.details
 import com.example.projektarb.data.moviedetails.MovieDetails
 import com.example.projektarb.remote.MovieInterface
 import com.example.projektarb.utils.Result
+import com.example.projektarb.utils.Status
 
 class MovieDetailsRepository ( private val movieInterface: MovieInterface) {
 
 
     suspend fun  getMovieDetails(imdbId:String): Result<MovieDetails>{
+
+
+        return try {
+
+            val response= movieInterface.getMovieDetails(imdbId)
+            if(response.isSuccessful){
+
+
+                Result(Status.SUCCESS,response.body(),null)
+
+            }else{
+                Result(Status.ERROR,null, null )
+            }
+
+
+
+        }catch (e:Exception){
+            Result(Status.ERROR,null, null )
+        }
+
 
     }
 
